1b8ebe2bf230d5db494f9a9aa9efa09203cd4231
appfromape <appfromape@gmail.com>
2021-03-15 21:49:15 +0800

    first commit

--- AppfromapeCard/ContentView.swift
+++ AppfromapeCard/ContentView.swift
@@ -9,13 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(UIColor(red: 0.09, green: 0.63, blue: 0.52, alpha: 0.60))
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("appfromape")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 160.0, height: 160.0)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().strokeBorder(Color.white, lineWidth: 5.0, antialiased: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                    )
+                Text("Appfromape")
+                    .font(Font.custom("Ranchers-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                Text("Python Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 20))
+                Divider()
+                InfoView(iconName: "phone.fill", showInfo: "+886912345678", iconColor: .green)
+                InfoView(iconName: "envelope.open.fill", showInfo: "appfromape@gmail.com", iconColor: .red)
+            }
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct InfoView: View {
+    let iconName : String
+    let showInfo : String
+    let iconColor : Color
+    var body: some View {
+        (Text(Image(systemName: iconName)).foregroundColor(iconColor) + Text(" ") + Text(showInfo))
+            .fixedSize(horizontal: false, vertical: true)
+            .multilineTextAlignment(.center)
+            .padding()
+            .frame(width: 250, height: 30)
+            .background(RoundedRectangle(cornerRadius: 50).fill(Color.white).shadow(radius: 3))
     }
 }
